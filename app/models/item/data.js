d1e0a73f0f091a4c867e04beceac5db3aacea91e
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-06-09 12:34:29 -0400

    Create data

---
+++ app/models/item/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'lait',
+    desc: '',
+    price: 4.99,
+    quantity: 1,
+    checked: false
+  },
+  {
+    id: 2,
+    name: 'pain',
+    desc: '',
+    price: 4.5,
+    quantity: 2,
+    checked: true
+  },
+  {
+    id: 3,
+    name: 'beurre',
+    desc: '',
+    price: 5.49,
+    quantity: 1,
+    checked: false
+  }
+];
